352cf9c3b1f565ec731343221534acc87d63f260
YunJaeHoon <sleepzzz214@naver.com>
2025-05-08 17:44:54 +0900

    fix: 메인 프레임 코드 정리

--- src/frame/MainFrame.java
+++ src/frame/MainFrame.java
@@ -14,10 +14,10 @@ import java.awt.*;
 
 public class MainFrame extends JFrame
 {
-	CanvasPanel canvasPanel;        	// 캔버스 패널
-	ToolbarPanel toolbarPanel;			// 도구 선택 패널
-	ColorPanel colorPanel;              // 색상 선택 패널
-	PropertyPanel propertyPanel;		// 속성 패널
+	CanvasPanel canvasPanel;		// 캔버스 패널
+	ToolbarPanel toolbarPanel;		// 도구 선택 패널
+	ColorPanel colorPanel;          // 색상 선택 패널
+	PropertyPanel propertyPanel;	// 속성 패널
 
 	// 싱글톤
 	public static MainFrame getInstance() { return MainFrame.SingleInstanceHolder.INSTANCE; }
@@ -26,7 +26,9 @@ public class MainFrame extends JFrame
 	// 생성자
 	private MainFrame()
 	{
-		// 기본 설정
+		/// 기본 설정
+
+		// 메인 프레임 설정
 		setTitle("그림을 그려보아요!");
 		setSize(800, 600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,23 +36,37 @@ public class MainFrame extends JFrame
 		// 컨텐츠 펜 설정
 		Container contentPane = this.getContentPane();
 		contentPane.setLayout(new BorderLayout());
+
+		/// 패널 생성
 		
-		// 캔버스 패널 생성
+		// 캔버스 패널
 		canvasPanel = CanvasPanelFactory.getInstance().createPanel();
 		
-		// 속성 패널 생성
+		// 속성 패널
 		propertyPanel = PropertyPanelFactory.getInstance().createPanel();
-		propertyPanel.addChangeComponentPropertyListener(canvasPanel);
-		canvasPanel.addComponentSelectionListener(propertyPanel);
-		
-		// 툴바 패널 생성
+
+		// 툴바 패널
 		toolbarPanel = ToolbarPanelFactory.getInstance().createPanel();
-		toolbarPanel.addToolSelectionListener(canvasPanel);
-		
-		// 컬러 패널 생성
+
+		// 컬러 패널
 		colorPanel = ColorPanelFactory.getInstance().createPanel();
+
+		/// 이벤트 리스너 등록
+
+		// 도구 선택 이벤트
+		toolbarPanel.addToolSelectionListener(canvasPanel);
+
+		// 색상 선택 이벤트
 		colorPanel.addColorSelectionListener(canvasPanel);
 		colorPanel.addColorSelectionListener(propertyPanel);
+
+		// 컴포넌트 속성 변경 이벤트
+		propertyPanel.addChangeComponentPropertyListener(canvasPanel);
+
+		// 컴포넌트 선택 이벤트
+		canvasPanel.addComponentSelectionListener(propertyPanel);
+
+		/// 패널 추가
 		
 		// 컨텐츠 펜에 패널 추가
 		contentPane.add(new JScrollPane(canvasPanel), BorderLayout.CENTER);
